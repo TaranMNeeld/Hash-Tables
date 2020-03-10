9756b72c8033a0a1da330c9bffcf0aa8377045a5
Taran <thetaranneeld@gmail.com>
2020-03-10 15:11:50 -0600

    refactored the retrieve function, and fixed remove function

--- src/hashtable.py
+++ src/hashtable.py
@@ -69,7 +69,6 @@ class HashTable:
             current.next = pair
             return
 
-
     def remove(self, key):
         '''
         Remove the value stored with the given key.
@@ -78,7 +77,18 @@ class HashTable:
 
         Fill this in.
         '''
-        pass
+        index = self._hash_mod(key)
+        if self.storage[index] is not None:
+            current = self.storage[index]
+            if key == self.storage[index].key:
+                self.storage[index] = None
+                return
+            while self.storage[index].next is not None:
+                self.storage[index] = self.storage[index].next
+                if key == self.storage[index].key:
+                    self.storage[index] = None
+                    return
+            return 'Item does not exist'
 
     def retrieve(self, key):
         '''
@@ -88,15 +98,15 @@ class HashTable:
 
         Fill this in.
         '''
-        for i in range(0, self.capacity):
-            if i == self._hash_mod(key) and self.storage[self._hash_mod(key)] is not None:
-                current = self.storage[i]
+        index = self._hash_mod(key)
+        if self.storage[index] is not None:
+            current = self.storage[index]
+            if key == current.key:
+                return current.value
+            while current.next is not None:
+                current = current.next
                 if key == current.key:
                     return current.value
-                while current.next is not None:
-                    current = current.next
-                    if key == current.key:
-                        return current.value
         return None
 
     def resize(self):
@@ -109,14 +119,14 @@ class HashTable:
         old_storage = self.storage.copy()
         self.capacity *= 2
         self.storage = [None] * self.capacity
-        for i in range(self.count):
-            current = old_storage[i]
-            if current is None:
-                pass
-            while current.next is not None:
-                current = current.next
-                print(f'key:{current.key} value:{current.value}')
-                self.storage.insert(current.key, current.value)
+        for item in old_storage:
+            if item is not None:
+                self.insert(item.key, item.value)
+                current = item
+                while current.next is not None:
+                    current = current.next
+                    print(f'key:{current.key} value:{current.value}')
+                    self.insert(current.key, current.value)
 
 
 if __name__ == "__main__":
@@ -130,7 +140,6 @@ if __name__ == "__main__":
     ht.insert("line_7", "Linked list saves the day!")
     ht.insert("line_8", "testing!")
     ht.insert("something", "Linked list saves the day!")
-
 
     print("")
 
